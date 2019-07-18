79bcd6ac56e901ccb4daed5c828a27ee34299499
Andrea Pascal <andrea@anodium.net>
2019-07-18 05:43:33 -0500

    Implement resume retrieval

--- src/zyciorys/core.cljs
+++ src/zyciorys/core.cljs
@@ -1,18 +1,28 @@
 (ns zyciorys.core
-    (:require
-      [reagent.core :as r]))
+  (:require
+   [reagent.core :as react]
+   [cljsjs.js-yaml :as yaml])) ; FIXME: Package not resolving
 
 ;; -------------------------
 ;; Views
 
-(defn home-page []
-  [:div [:h2 "Welcome to Raehduigent"]])
+(defn home-page [resume]
+  [:div [:h2 resume]])
 
 ;; -------------------------
 ;; Initialize app
+(def ^:dynamic *resume*
+  (.. (.. js/window (fetch
+                     (.. js/document (getElementById "resume")
+                         (getAttribute "href"))))
+      then (fn [declare response] ; FIXME: Variable not binding
+             ((println response)
+             (if (.. response Ok)
+               (println "I'm OKAY!")
+               (println "... Houston?"))))))
 
 (defn mount-root []
-  (r/render [home-page] (.getElementById js/document "app")))
+  (react/render [home-page] (.. js/document (getElementById "app"))))
 
 (defn init! []
   (mount-root))
