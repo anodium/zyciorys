eaa9ec1c19675c72e3b161f6def4bb9cd08b42d2
Andrea Pascal <andrea@anodium.net>
2019-07-17 20:56:16 -0500

    Add info to project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject zyciorys "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Dynamic résumé renderer. Written because I hate typesetting."
+  :url "https://github.com/anodium/zyciorys"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.339"]]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [io.forward/yaml "1.0.9"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.2"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
